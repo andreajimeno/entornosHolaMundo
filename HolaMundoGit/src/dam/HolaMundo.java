5917e671108be64102a804e1fc488e93231cc318
Andrea <andreajimeno6@gmail.com>
2021-04-21 10:12:46 +0200

    bucle de mensajes

--- HolaMundoGit/src/dam/HolaMundo.java
+++ HolaMundoGit/src/dam/HolaMundo.java
@@ -4,7 +4,12 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola mundo ");
+		int cont = 1;
+		while (cont <= 10) {
+			System.out.println("Hola mundo ");
+			cont++;
+		}
+		System.out.println("Fin");
 	}
 
 }
