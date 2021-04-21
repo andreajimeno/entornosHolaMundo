cc1be4df49d6e5ca9e232822577c8e1d94de4fd1
Andrea <andreajimeno6@gmail.com>
2021-04-21 10:28:41 +0200

    cambio de formato

--- HolaMundoGit/src/dam/HolaMundo.java
+++ HolaMundoGit/src/dam/HolaMundo.java
@@ -4,7 +4,9 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		
+		
 		int cont = 1;
+		System.out.printf(" %4d > Hola mundo\n", cont);
 		while (cont <= 10) {
 			System.out.println("Hola mundo ");
 			cont++;
